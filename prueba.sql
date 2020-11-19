1223d32f926160df04fa71709eb31e5d581fd122
alfredopl <aalfredoplz@gmail.com>
2020-11-19 15:16:35 -0300

    insert data in product

--- prueba.sql
+++ prueba.sql
@@ -15,7 +15,7 @@ CREATE TABLE producto(
     id INT PRIMARY KEY,
     nombre VARCHAR (150),
     descripcion VARCHAR (200),
-    valor REAL
+    valor INT
 );
 
 --creamos la tabla Factura
@@ -65,3 +65,20 @@ VALUES ('Maria Perez', '26442334-5', 'San Miguel');
 INSERT INTO cliente (nombre, rut, direccion)
 VALUES ('Lucia Rojas', '21341334-1', 'Estacion Central');
 
+--insertamos 8 productos en la tabla producto
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (200 , 'Doritos', 'tradicional queso 320gr', 1180);
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (300 , 'Ruffles', 'Mega crunch 120gr', 800);
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (400 , 'Cheetos', 'Palitos sabor queso', 1000);
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (500 , "Lay's", 'Original 120gr', 750);
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (600 , 'Oreo', 'Galleta vainilla con relleno vanilla 126gr', 600);
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (700 , 'Pepsi 3l', 'Bebida light 3lts', 2050);
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (800 , 'Coca-cola 3l', 'Bebida original 3lts', 1950);
+INSERT INTO producto (id ,nombre , descripcion, valor)
+VALUES (900 , 'Mani 430g', 'Mani sin sal 430g', 1800);
